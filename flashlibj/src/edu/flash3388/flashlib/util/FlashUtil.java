37153fa27997a151643464cd450d552f389f1669
Tom Tzook <tomtzook@gmail.com>
2017-12-08 18:30:34 +0200

    Updated FlashLib version constant to 1.2.1

--- flashlibj/src/edu/flash3388/flashlib/util/FlashUtil.java
+++ flashlibj/src/edu/flash3388/flashlib/util/FlashUtil.java
@@ -40,7 +40,7 @@ public final class FlashUtil {
 	/**
 	 * The current version of FlashLib.
 	 */
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 	
 	private FlashUtil(){}
 	
