011cc04fde44ac44a86a2bb2ef65ee8214cf6296
Tom Tzook <tomtzook@gmail.com>
2018-03-31 11:25:40 +0300

    Changed FlashUtil version variable

--- flashlibj/src/edu/flash3388/flashlib/util/FlashUtil.java
+++ flashlibj/src/edu/flash3388/flashlib/util/FlashUtil.java
@@ -41,7 +41,7 @@ public final class FlashUtil {
 	/**
 	 * The current version of FlashLib.
 	 */
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.3.0";
 	
 	private FlashUtil(){}
 	
