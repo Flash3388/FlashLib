6497f42a42001bafdf9c2b6ea6db61d9742d859f
Tom Tzook <tomtzook@gmail.com>
2018-06-23 23:49:30 +0300

    message reader/writer working with serializer interface

--- flashlibj/src/edu/flash3388/flashlib/communications/message/MessageReader.java
+++ flashlibj/src/edu/flash3388/flashlib/communications/message/MessageReader.java
@@ -2,27 +2,27 @@ package edu.flash3388.flashlib.communications.message;
 
 import java.io.IOException;
 
-import com.tomtzook.antman.communication.StaticMessage;
-
 import edu.flash3388.flashlib.communications.connection.Connection;
 import edu.flash3388.flashlib.communications.connection.TimeoutException;
-import edu.flash3388.flashlib.util.FlashUtil;
+import edu.flash3388.flashlib.io.PrimitiveSerializer;
 
 public class MessageReader {
 
 	private Connection mConnection;
+	private PrimitiveSerializer mSerializer;
 	
-	public MessageReader(Connection connection) {
+	public MessageReader(Connection connection, PrimitiveSerializer serializer) {
 		mConnection = connection;
+		mSerializer = serializer;
 	}
 	
 	public Message readMessage() throws ReadException, TimeoutException {
 		try {
 			byte[] headerData = mConnection.read(4);
-			int header = FlashUtil.toInt(headerData);
+			int header = mSerializer.toInt(headerData);
 			
 			byte[] lengthData = mConnection.read(4);
-			int length = FlashUtil.toInt(lengthData);
+			int length = mSerializer.toInt(lengthData);
 			
 			byte[] data = mConnection.read(length);
 			return new StaticMessage(header, data);
--- flashlibj/src/edu/flash3388/flashlib/communications/message/MessageWriter.java
+++ flashlibj/src/edu/flash3388/flashlib/communications/message/MessageWriter.java
@@ -3,20 +3,22 @@ package edu.flash3388.flashlib.communications.message;
 import java.io.IOException;
 
 import edu.flash3388.flashlib.communications.connection.Connection;
-import edu.flash3388.flashlib.util.FlashUtil;
+import edu.flash3388.flashlib.io.PrimitiveSerializer;
 
 public class MessageWriter {
 
 	private Connection mConnection;
+	private PrimitiveSerializer mSerializer;
 	
-	public MessageWriter(Connection connection) {
+	public MessageWriter(Connection connection, PrimitiveSerializer serializer) {
 		mConnection = connection;
+		mSerializer = serializer;
 	}
 	
 	public void writeMessage(Message message) throws WriteException {
-		byte[] header = FlashUtil.toByteArray(message.getHeader());
+		byte[] header = mSerializer.toBytes(message.getHeader());
 		byte[] data = message.getData();
-		byte[] lengthData = FlashUtil.toByteArray(data.length);
+		byte[] lengthData = mSerializer.toBytes(data.length);
 		
 		byte[] sendData = new byte[header.length + 4 + data.length];
 		System.arraycopy(header, 0, sendData, 0, header.length);
