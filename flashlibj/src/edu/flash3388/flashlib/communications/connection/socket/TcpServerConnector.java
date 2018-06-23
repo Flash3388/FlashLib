3d65f35e20f42468e78e295240eb532098fad7e0
Tom Tzook <tomtzook@gmail.com>
2018-06-24 00:10:46 +0300

    Added tcp server connector

--- flashlibj/src/edu/flash3388/flashlib/communications/connection/socket/TcpServerConnector.java
+++ flashlibj/src/edu/flash3388/flashlib/communications/connection/socket/TcpServerConnector.java
@@ -1,30 +1,29 @@
 package edu.flash3388.flashlib.communications.connection.socket;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketAddress;
 
 import edu.flash3388.flashlib.communications.connection.Connection;
 import edu.flash3388.flashlib.communications.connection.ConnectionFailedException;
 import edu.flash3388.flashlib.communications.connection.Connector;
 import edu.flash3388.flashlib.io.Closer;
 
-public class TcpClientConnector implements Connector {
+public class TcpServerConnector implements Connector {
 
-	private SocketAddress mEndPoint;
+	private ServerSocket mServerSocket;
 	private int mReadTimeout;
 	
-	public TcpClientConnector(SocketAddress endPoint, int readTimeout) {
-		mEndPoint = endPoint;
+	public TcpServerConnector(ServerSocket serverSocket, int readTimeout) {
+		mServerSocket = serverSocket;
 		mReadTimeout = readTimeout;
 	}
-	
+
 	@Override
 	public Connection connect(int connectionTimeout) throws ConnectionFailedException {
-		Socket socket = new Socket();
 		try {
-			return Closer.onError(socket).run(()->{
-				socket.connect(mEndPoint, connectionTimeout);
+			Socket socket = mServerSocket.accept();
+			return Closer.onError(socket).run(()-> {
 				socket.setSoTimeout(mReadTimeout);
 				
 				return new SocketConnection(socket);
@@ -33,5 +32,4 @@ public class TcpClientConnector implements Connector {
 			throw new ConnectionFailedException(e);
 		}
 	}
-
 }
