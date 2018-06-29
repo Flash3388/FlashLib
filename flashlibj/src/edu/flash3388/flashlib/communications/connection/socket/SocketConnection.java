e6049d409f086570aa7c67f6c64de1d79ecfc1a6
Tom Tzook <tomtzook@gmail.com>
2018-06-29 19:15:14 +0300

    Updated connection implementation to use DataInputStream wrapper

--- flashlibj/src/edu/flash3388/flashlib/communications/connection/socket/SocketConnection.java
+++ flashlibj/src/edu/flash3388/flashlib/communications/connection/socket/SocketConnection.java
@@ -1,5 +1,6 @@
 package edu.flash3388.flashlib.communications.connection.socket;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,13 +15,13 @@ public class SocketConnection implements Connection {
 	private Socket mSocket;
 	
 	private OutputStream mOut;
-	private InputStream mIn;
+	private DataInputStream mIn;
 	
 	public SocketConnection(Socket socket) throws IOException {
 		mSocket = socket;
 		
 		mOut = mSocket.getOutputStream();
-		mIn = mSocket.getInputStream();
+		mIn = new DataInputStream(mSocket.getInputStream());
 	}
 	
 	@Override
@@ -32,7 +33,7 @@ public class SocketConnection implements Connection {
 	public byte[] read(int count) throws IOException, TimeoutException {
 		try {
 			byte[] buffer = new byte[count];
-			mIn.read(buffer, 0, count);
+			mIn.readFully(buffer, 0, count);
 			
 			return buffer;
 		} catch (SocketTimeoutException e) {
