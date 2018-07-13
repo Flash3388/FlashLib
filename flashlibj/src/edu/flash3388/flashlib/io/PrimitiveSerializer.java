022b71b846e876d178ba95b3756ed45b1b370ba7
Tom Tzook <tomtzook@gmail.com>
2018-07-13 19:01:20 +0300

    added offsets to serializing

--- flashlibj/src/edu/flash3388/flashlib/io/PrimitiveSerializer.java
+++ flashlibj/src/edu/flash3388/flashlib/io/PrimitiveSerializer.java
@@ -8,9 +8,28 @@ public interface PrimitiveSerializer {
 	byte[] toBytes(float value);
 	byte[] toBytes(double value);
 	
-	short toShort(byte[] bytes);
-	int toInt(byte[] bytes);
-	long toLong(byte[] bytes);
-	float toFloat(byte[] bytes);
-	double toDouble(byte[] bytes);
+	short toShort(byte[] bytes, int offset);
+	default short toShort(byte[] bytes) {
+		return toShort(bytes, 0);
+	}
+
+	int toInt(byte[] bytes, int offset);
+	default int toInt(byte[] bytes) {
+		return toInt(bytes, 0);
+	}
+
+	long toLong(byte[] bytes, int offset);
+	default long toLong(byte[] bytes) {
+		return toLong(bytes, 0);
+	}
+
+	float toFloat(byte[] bytes, int offset);
+	default float toFloat(byte[] bytes) {
+		return toFloat(bytes, 0);
+	}
+
+	double toDouble(byte[] bytes, int offset);
+	default double toDouble(byte[] bytes) {
+		return toDouble(bytes, 0);
+	}
 }
