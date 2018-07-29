3bbbffde55bbab03b1abaac3a7c6f0560b08927f
Tom Tzook <tomtzook@gmail.com>
2018-07-29 23:27:29 +0300

    resource is now autocloseable

--- flashlibj/src/edu/flash3388/flashlib/util/Resource.java
+++ flashlibj/src/edu/flash3388/flashlib/util/Resource.java
@@ -1,7 +1,12 @@
 package edu.flash3388.flashlib.util;
 
 @FunctionalInterface
-public interface Resource {
+public interface Resource extends AutoCloseable {
 
     void free();
+
+    @Override
+    default void close() {
+        free();
+    }
 }
