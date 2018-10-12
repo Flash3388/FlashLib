263f66b1fdf8cdcb76e82a296983fa83df36f137
Tom Tzook <tomtzook@gmail.com>
2018-10-12 20:32:16 +0300

    povrange implementations to lamda

--- flashlibj/src/main/java/edu/flash3388/flashlib/robot/hid/POVRange.java
+++ flashlibj/src/main/java/edu/flash3388/flashlib/robot/hid/POVRange.java
@@ -3,40 +3,15 @@ package edu.flash3388.flashlib.robot.hid;
 @FunctionalInterface
 public interface POVRange {
 
-    public static final POVRange UP = new POVRange() {
-        @Override
-        public boolean isInRange(int degrees) {
-            return (degrees >= 315 || degrees <= 45) && degrees > 0;
-        }
-    };
+    POVRange UP = degrees -> (degrees >= 315 || degrees <= 45) && degrees > 0;
 
-    public static final POVRange DOWN = new POVRange() {
-        @Override
-        public boolean isInRange(int degrees) {
-            return degrees >= 135 && degrees <= 255;
-        }
-    };
+    POVRange DOWN = degrees -> degrees >= 135 && degrees <= 255;
 
-    public static final POVRange LEFT = new POVRange() {
-        @Override
-        public boolean isInRange(int degrees) {
-            return degrees >= 255 && degrees <= 315;
-        }
-    };
+    POVRange LEFT = degrees -> degrees >= 255 && degrees <= 315;
 
-    public static final POVRange RIGHT = new POVRange() {
-        @Override
-        public boolean isInRange(int degrees) {
-            return degrees >= 45 && degrees <= 135;
-        }
-    };
+    POVRange RIGHT = degrees -> degrees >= 45 && degrees <= 135;
 
-    public static final POVRange FULL = new POVRange() {
-        @Override
-        public boolean isInRange(int degrees) {
-            return degrees > 0;
-        }
-    };
+    POVRange FULL = degrees -> degrees > 0;
 
     boolean isInRange(int degrees);
 }
