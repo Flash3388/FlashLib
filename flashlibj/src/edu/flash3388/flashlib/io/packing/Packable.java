2e7cf9246dc08348eabb2424d5092cb9ec85fb0e
Tom Tzook <tomtzook@gmail.com>
2018-10-12 13:35:49 +0300

    removed packing : unused

--- flashlibj/src/edu/flash3388/flashlib/io/packing/Packable.java
+++
@@ -1,8 +0,0 @@
-package edu.flash3388.flashlib.io.packing;
-
-import java.io.IOException;
-
-public interface Packable {
-    void pack(DataPacker packer) throws IOException;
-    void unpack(DataUnpacker unpacker) throws IOException;
-}
