b0526926c09efa72ddfa43864b302a3126ddb707
Tom Tzook <tomtzook@gmail.com>
2018-10-12 13:29:57 +0300

    flashlib version using versioning

--- flashlibj/src/edu/flash3388/flashlib/FlashLib.java
+++ flashlibj/src/edu/flash3388/flashlib/FlashLib.java
@@ -1,10 +1,12 @@
 package edu.flash3388.flashlib;
 
+import edu.flash3388.flashlib.util.versioning.Version;
+
 public class FlashLib {
     private FlashLib() {}
 
     /**
      * The current version of FlashLib.
      */
-    public static final String VERSION = "1.3.0";
+    public static final Version VERSION = new Version(1, 3, 0);
 }
