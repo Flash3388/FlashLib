3e7b3326964402c67f3ae8dcdf7ed3596fc78a5e
Tom Tzook <tomtzook@gmail.com>
2018-10-12 20:58:11 +0300

    no such stick exception deleted

--- flashlibj/src/main/java/edu/flash3388/flashlib/robot/hid/NoSuchStickException.java
+++
@@ -1,8 +0,0 @@
-package edu.flash3388.flashlib.robot.hid;
-
-public class NoSuchStickException extends RuntimeException {
-
-    public NoSuchStickException(int channel, int stick) {
-        super(String.format("No stick %d for channel %d", stick, channel));
-    }
-}
