0be94c0d13c247e8b7980c3ec01443ff897b9397
Tom Tzook <tomtzook@gmail.com>
2018-10-27 16:34:45 +0300

    removed unused resource

--- flashlibj/src/test/java/edu/flash3388/flashlib/util/resources/ResourceHolderTest.java
+++ flashlibj/src/test/java/edu/flash3388/flashlib/util/resources/ResourceHolderTest.java
@@ -1,7 +1,5 @@
 package edu.flash3388.flashlib.util.resources;
 
-import edu.flash3388.flashlib.util.resources.Resource;
-import edu.flash3388.flashlib.util.resources.ResourceHolder;
 import org.junit.Test;
 
 import static org.junit.Assert.fail;
