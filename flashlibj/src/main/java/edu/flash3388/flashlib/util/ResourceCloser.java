a303296bd555dcd8fc51cff02338c191c3bd3904
Tom Tzook <tomtzook@gmail.com>
2018-10-27 15:34:34 +0300

    added simple resource closer

---
+++ flashlibj/src/main/java/edu/flash3388/flashlib/util/ResourceCloser.java
@@ -0,0 +1,33 @@
+package edu.flash3388.flashlib.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class ResourceCloser {
+
+    private final Collection<Resource> mResources;
+
+    private ResourceCloser(Collection<Resource> resources) {
+        mResources = resources;
+    }
+
+    public static ResourceCloser empty() {
+        return new ResourceCloser(new ArrayList<Resource>());
+    }
+
+    public static ResourceCloser with(Resource... resources) {
+        return new ResourceCloser(Arrays.asList(resources));
+    }
+
+    public ResourceCloser add(Resource resource) {
+        mResources.add(resource);
+        return this;
+    }
+
+    public void close() {
+        for (Resource resource : mResources) {
+            resource.close();
+        }
+    }
+}
