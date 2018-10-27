bc964b8d8c03137e41ff9782d5d4392855d1f1c8
Tom Tzook <tomtzook@gmail.com>
2018-10-27 14:37:37 +0300

    hidscheduling helper util

---
+++ flashlibj/src/main/java/edu/flash3388/flashlib/robot/hid/scheduling/HidScheduling.java
@@ -0,0 +1,18 @@
+package edu.flash3388.flashlib.robot.hid.scheduling;
+
+import edu.flash3388.flashlib.robot.hid.Button;
+import edu.flash3388.flashlib.robot.hid.ButtonSchedulerTask;
+import edu.flash3388.flashlib.robot.hid.Hid;
+import edu.flash3388.flashlib.robot.scheduling.Scheduler;
+
+public class HidScheduling {
+
+    private HidScheduling() {}
+
+    public static void addButtonsUpdateTaskToScheduer(Hid hid, Scheduler scheduler) {
+        for (int buttonIdx = 0; buttonIdx < hid.getButtonCount(); buttonIdx++) {
+            Button button = hid.getButton(buttonIdx);
+            scheduler.add(new ButtonSchedulerTask(button));
+        }
+    }
+}
