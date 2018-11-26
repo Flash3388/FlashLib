dfdba731a0ed374f8fd79461aad58313cd0d713d
Tom Tzook <tomtzook@gmail.com>
2018-11-26 23:22:08 +0200

    default methods in movable

--- flashlibj/src/main/java/edu/flash3388/flashlib/robot/motion/Movable.java
+++ flashlibj/src/main/java/edu/flash3388/flashlib/robot/motion/Movable.java
@@ -3,4 +3,16 @@ package edu.flash3388.flashlib.robot.motion;
 public interface Movable extends Stoppable {
 
     void move(double speed);
+
+    default void move(double speed, Direction direction) {
+        move(Math.abs(speed) * direction.sign());
+    }
+
+    default void forward(double speed) {
+        move(speed, Direction.FORWARD);
+    }
+
+    default void backward(double speed) {
+        move(speed, Direction.BACKWARD);
+    }
 }
