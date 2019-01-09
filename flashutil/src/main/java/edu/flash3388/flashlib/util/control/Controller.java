41ffa502ed9a52ffd3f74cab09f978d9a915921b
Tom Tzook <tomtzook@gmail.com>
2019-01-09 23:37:35 +0200

    image delegation runner

---
+++ flashutil/src/main/java/edu/flash3388/flashlib/util/control/Controller.java
@@ -0,0 +1,9 @@
+package edu.flash3388.flashlib.util.control;
+
+public interface Controller {
+
+    boolean isRunning();
+
+    void start();
+    void stop();
+}
---
+++ flashutil/src/main/java/edu/flash3388/flashlib/util/control/SingleUseController.java
@@ -0,0 +1,60 @@
+package edu.flash3388.flashlib.util.control;
+
+import java.io.Closeable;
+
+public abstract class SingleUseController implements Controller, Closeable {
+
+    private volatile boolean mIsRunning;
+    private volatile boolean mIsTerminated;
+
+    public SingleUseController() {
+        mIsRunning = false;
+        mIsTerminated = false;
+    }
+
+    @Override
+    public final boolean isRunning() {
+        return mIsRunning;
+    }
+
+    public final boolean isTerminated() {
+        return mIsTerminated;
+    }
+
+    @Override
+    public final synchronized void start() {
+        if (isTerminated()) {
+            throw new IllegalStateException("terminated");
+        }
+        if (isRunning()) {
+            throw new IllegalStateException("already running");
+        }
+
+        startController();
+
+        mIsRunning = true;
+    }
+
+    @Override
+    public final synchronized void stop() {
+        if (isTerminated()) {
+            throw new IllegalStateException("already terminated");
+        }
+        if (!isRunning()) {
+            throw new IllegalStateException("not running");
+        }
+
+        stopController();
+
+        mIsTerminated = true;
+        mIsRunning = false;
+    }
+
+    @Override
+    public final void close() {
+        stop();
+    }
+
+    protected abstract void startController();
+    protected abstract void stopController();
+}
