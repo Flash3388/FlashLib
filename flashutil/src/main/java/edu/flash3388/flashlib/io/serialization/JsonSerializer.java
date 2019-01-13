37cb6c2f83037c8799affd48810ac9c40eb47d7d
Tom Tzook <tomtzook@gmail.com>
2019-01-13 23:34:35 +0200

    jsonserializer

---
+++ flashutil/src/main/java/edu/flash3388/flashlib/io/serialization/JsonSerializer.java
@@ -0,0 +1,41 @@
+package edu.flash3388.flashlib.io.serialization;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class JsonSerializer implements Serializer {
+
+    private final Gson mGson;
+    private final Charset mCharset;
+
+    public JsonSerializer(Gson gson, Charset charset) {
+        mGson = gson;
+        mCharset = charset;
+    }
+
+    @Override
+    public <T> byte[] serialize(T value) throws IOException {
+        try {
+            String json = mGson.toJson(value);
+            return json.getBytes(mCharset);
+        } catch (JsonIOException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public <T> T deserialize(byte[] serializedValue, Class<T> type) throws IOException, TypeException {
+        try {
+            Object value = mGson.fromJson(new String(serializedValue, mCharset), Object.class);
+            return type.cast(value);
+        } catch (JsonSyntaxException | JsonIOException e) {
+            throw new IOException(e);
+        } catch (ClassCastException e) {
+            throw new TypeException(e);
+        }
+    }
+}
---
+++ flashutil/src/test/java/edu/flash3388/flashlib/io/serialization/JsonSerializerTest.java
@@ -0,0 +1,58 @@
+package edu.flash3388.flashlib.io.serialization;
+
+import com.google.gson.Gson;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.assertEquals;
+
+public class JsonSerializerTest {
+
+    private static final double DOUBLE_EQUALS_MARGIN = 0.0001;
+
+    @Test
+    public void serialization_objectStateRemains() throws Exception {
+        final TestClass OBJECT = new TestClass(23, 5.0);
+
+        JsonSerializer jsonSerializer = new JsonSerializer(new Gson(), Charset.forName("UTF-8"));
+        byte[] serialized = jsonSerializer.serialize(OBJECT);
+        TestClass deserializedObject = jsonSerializer.deserialize(serialized, TestClass.class);
+
+        assertTestClassEquals(OBJECT, deserializedObject);
+    }
+
+    @Test(expected = TypeException.class)
+    public void deserialize_classTypeDoesNotMatch_throwsTypeException() throws Exception {
+        final TestClass OBJECT = new TestClass(23, 5.0);
+
+        JsonSerializer serializer = new JsonSerializer(new Gson(), Charset.forName("UTF-8"));
+
+        byte[] serializedValue = serializer.serialize(OBJECT);
+        serializer.deserialize(serializedValue, OtherObject.class);
+    }
+
+    private void assertTestClassEquals(TestClass expected, TestClass actual) {
+        assertEquals(expected.variable1, actual.variable1);
+        assertEquals(expected.variable2, actual.variable2, DOUBLE_EQUALS_MARGIN);
+    }
+
+    private static class TestClass {
+
+        private final int variable1;
+        private double variable2;
+
+        TestClass(int variable1, double variable2) {
+            this.variable1 = variable1;
+            this.variable2 = variable2;
+        }
+
+        public boolean equals(TestClass testClass) {
+            return variable1 == testClass.variable1 && variable2 == testClass.variable2;
+        }
+    }
+
+    private static class OtherObject {
+
+    }
+}
