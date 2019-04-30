58190f7dd21e7fbc3c962fe579dc1d32d6f146d7
Tom Tzook <tomtzook@gmail.com>
2019-04-30 22:11:25 +0300

    removed unused executor service in mjpegserver

--- flashvision/src/main/java/com/flash3388/flashlib/vision/jpeg/server/MjpegServer.java
+++ flashvision/src/main/java/com/flash3388/flashlib/vision/jpeg/server/MjpegServer.java
@@ -1,37 +1,32 @@
 package com.flash3388.flashlib.vision.jpeg.server;
 
 import com.flash3388.flashlib.io.Closer;
-import com.flash3388.flashlib.util.concurrent.ExecutorCloser;
+import com.flash3388.flashlib.time.Clock;
 import com.flash3388.flashlib.util.http.HttpServerCloser;
+import com.flash3388.flashlib.vision.camera.Camera;
 import com.flash3388.flashlib.vision.jpeg.JpegImage;
 import com.sun.net.httpserver.HttpServer;
-import com.flash3388.flashlib.time.Clock;
-import com.flash3388.flashlib.vision.camera.Camera;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MjpegServer implements Closeable {
 
     private final HttpServer mServer;
-    private final ExecutorService mExecutorService;
     private final Clock mClock;
     private final Logger mLogger;
 
-    public MjpegServer(HttpServer server, ExecutorService executorService, Clock clock, Logger logger) {
+    public MjpegServer(HttpServer server, Clock clock, Logger logger) {
         mServer = server;
-        mExecutorService = executorService;
         mClock = clock;
         mLogger = logger;
     }
 
     public static MjpegServer create(InetSocketAddress serverAddress, int handlerThreads, Clock clock, Logger logger) throws IOException {
-        return new MjpegServer(HttpServer.create(serverAddress, handlerThreads), Executors.newFixedThreadPool(handlerThreads), clock, logger);
+        return new MjpegServer(HttpServer.create(serverAddress, handlerThreads), clock, logger);
     }
 
     public void start() {
@@ -51,7 +46,6 @@ public class MjpegServer implements Closeable {
         try {
             Closer closer = Closer.empty();
 
-            closer.add(new ExecutorCloser(mExecutorService));
             closer.add(new HttpServerCloser(mServer));
 
             closer.close();
