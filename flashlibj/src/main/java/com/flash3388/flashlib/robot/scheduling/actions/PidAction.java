ae5789c79b63828b224a19817148dc45683a6470
Tom Tzook <tomtzook@gmail.com>
2019-05-10 20:33:22 +0300

    added pid action

---
+++ flashlibj/src/main/java/com/flash3388/flashlib/robot/scheduling/actions/PidAction.java
@@ -0,0 +1,54 @@
+package com.flash3388.flashlib.robot.scheduling.actions;
+
+import com.flash3388.flashlib.robot.control.PidController;
+import com.flash3388.flashlib.robot.motion.Movable;
+import com.flash3388.flashlib.robot.scheduling.Action;
+import com.jmath.ExtendedMath;
+
+import java.util.function.DoubleSupplier;
+
+public class PidAction extends Action {
+
+    private final PidController mPidController;
+    private final Movable mMovable;
+    private final DoubleSupplier mProcessVariableSupplier;
+    private final DoubleSupplier mSetPointSupplier;
+    private final double mThresholdMargin;
+
+    public PidAction(PidController pidController, Movable movable, DoubleSupplier processVariableSupplier, DoubleSupplier setPointSupplier, double thresholdMargin) {
+        mPidController = pidController;
+        mMovable = movable;
+        mProcessVariableSupplier = processVariableSupplier;
+        mSetPointSupplier = setPointSupplier;
+        mThresholdMargin = thresholdMargin;
+    }
+
+    @Override
+    protected void initialize() {
+        mPidController.reset();
+    }
+
+    @Override
+    protected void execute() {
+        double value = mPidController.calculate(
+                mProcessVariableSupplier.getAsDouble(),
+                mSetPointSupplier.getAsDouble());
+
+        mMovable.move(value);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        double processVariable = mProcessVariableSupplier.getAsDouble();
+
+        return ExtendedMath.constrained(
+                mProcessVariableSupplier.getAsDouble(),
+                processVariable - mThresholdMargin,
+                processVariable + mThresholdMargin);
+    }
+
+    @Override
+    protected void end() {
+        mMovable.stop();
+    }
+}
