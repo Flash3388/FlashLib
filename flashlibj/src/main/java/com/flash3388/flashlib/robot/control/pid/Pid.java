e1dcc1812069c1f125f4cfb6e4b0f9564e403170
Tom Tzook <tomtzook@gmail.com>
2019-05-11 18:39:33 +0300

    working on pid interface

---
+++ flashlibj/src/main/java/com/flash3388/flashlib/robot/control/pid/Pid.java
@@ -0,0 +1,18 @@
+package com.flash3388.flashlib.robot.control.pid;
+
+public interface Pid {
+
+    double getP();
+    double getI();
+    double getD();
+
+    void setP(double p);
+    void setI(double i);
+    void setD(double d);
+
+    void setOutputLimit(double min, double max);
+
+    void reset();
+
+    double calculate(double processVariable, double setPoint);
+}
---
+++ flashlibj/src/main/java/com/flash3388/flashlib/robot/control/pid/PidBase.java
@@ -0,0 +1,63 @@
+package com.flash3388.flashlib.robot.control.pid;
+
+import com.beans.DoubleProperty;
+import com.beans.properties.SimpleDoubleProperty;
+
+public abstract class PidBase implements Pid {
+
+    private final DoubleProperty mKp;
+    private final DoubleProperty mKi;
+    private final DoubleProperty mKd;
+
+    protected PidBase(DoubleProperty kp, DoubleProperty ki, DoubleProperty kd) {
+        mKp = kp;
+        mKi = ki;
+        mKd = kd;
+    }
+
+    protected PidBase(double kp, double ki, double kd) {
+        this(new SimpleDoubleProperty(kp), new SimpleDoubleProperty(ki), new SimpleDoubleProperty(kd));
+    }
+
+    public DoubleProperty kpProperty() {
+        return mKp;
+    }
+
+    @Override
+    public void setP(double p) {
+        mKp.setAsDouble(p);
+    }
+
+    @Override
+    public double getP() {
+        return mKp.getAsDouble();
+    }
+
+    public DoubleProperty kiProperty() {
+        return mKi;
+    }
+
+    @Override
+    public void setI(double i) {
+        mKi.setAsDouble(i);
+    }
+
+    @Override
+    public double getI() {
+        return mKi.getAsDouble();
+    }
+
+    public DoubleProperty kdProperty() {
+        return mKd;
+    }
+
+    @Override
+    public void setD(double d) {
+        mKd.setAsDouble(d);
+    }
+
+    @Override
+    public double getD() {
+        return mKd.getAsDouble();
+    }
+}
