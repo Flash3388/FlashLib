1730406fc30deb3a3c426a2d4f5537e0c5e8ed07
Tom Tzook <tomtzook@gmail.com>
2019-07-26 15:11:41 +0300

    analysis uses map data and not json

--- flashvision/src/main/java/com/flash3388/flashlib/vision/processing/analysis/Analysis.java
+++ flashvision/src/main/java/com/flash3388/flashlib/vision/processing/analysis/Analysis.java
@@ -1,16 +1,42 @@
 package com.flash3388.flashlib.vision.processing.analysis;
 
-import com.google.gson.JsonObject;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Analysis {
 
-    private final JsonObject mData;
+    public static class Builder {
 
-    public Analysis(JsonObject data) {
-        mData = data;
+        private final Map<String, Object> mData;
+
+        public Builder(Map<String, Object> data) {
+            mData = data;
+        }
+
+        public Builder() {
+            this(new HashMap<>());
+        }
+
+        public Builder put(String key, Object value) {
+            mData.put(key, value);
+            return this;
+        }
+
+        public Analysis build() {
+            return new Analysis(mData);
+        }
     }
 
-    public JsonObject getData() {
+    private final Map<String, Object> mData;
+
+    public Analysis(Map<String, Object> data) {
+        Objects.requireNonNull(data, "data is null");
+        mData = Collections.unmodifiableMap(data);
+    }
+
+    public Map<String, Object> getData() {
         return mData;
     }
 
