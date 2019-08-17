771c515ef8b460428e719cf31d08f5709b7210c0
Tom Tzook <tomtzook@gmail.com>
2019-08-17 04:55:55 +0300

    testing deploy fix

--- travis-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-./gradlew publish -PNEXUS_USERNAME=${NEXUS_USERNAME} -PNEXUS_PASSWORD=${NEXUS_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=secring.gpg
-
-
