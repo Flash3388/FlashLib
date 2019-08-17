14d123b9b49b7d796a824e2d3b843800fd44549d
Tom Tzook <tomtzook@gmail.com>
2019-08-17 04:39:55 +0300

    /working on nexus deployment

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K 
-  ./gradlew publish -PNEXUS_USERNAME=${NEXSUS_USERNAME} -PNEXUS_PASSWORD=${NEXUS_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=secring.gpg
-fi
+./gradlew publish -PNEXUS_USERNAME=${NEXSUS_USERNAME} -PNEXUS_PASSWORD=${NEXUS_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=secring.gpg
 
 
