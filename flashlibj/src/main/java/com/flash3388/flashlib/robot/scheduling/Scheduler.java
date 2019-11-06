ba2bb9d370a1e3c4202b5423ac5158f3df70cd20
Tom Tzook <tomtzook@gmail.com>
2019-11-06 17:58:22 +0200

    quick formatting

--- flashlibj/src/main/java/com/flash3388/flashlib/robot/scheduling/Scheduler.java
+++ flashlibj/src/main/java/com/flash3388/flashlib/robot/scheduling/Scheduler.java
@@ -11,24 +11,27 @@ import java.util.function.Predicate;
  * <p>
  * For more complex operations, the scheduler can use {@link Action} objects. Those objects can be added
  * to the scheduler and then executed as well. Unlike simple tasks, scheduling might depend on {@link Subsystem}
- * objects for operations. The scheduler tracks the required systems of each action making sure that only one 
- * {@link Action} object runs on a {@link Subsystem} at any given time. 
+ * objects for operations. The scheduler tracks the required systems of each action making sure that only one
+ * {@link Action} object runs on a {@link Subsystem} at any given time.
  * <p>
  * In addition, the scheduler can allow {@link Subsystem} to hold default {@link Action} objects, which
  * run only if no {@link Action} is using the {@link Subsystem} at the moment. When the scheduler runs,
  * it checks all registered {@link Subsystem} objects to see if one does not have an action at the moment.
- * 
+ *
  * @author Tom Tzook
  * @since FlashLib 1.0.0
  */
 public interface Scheduler {
 
-	void add(Action action);
+    void add(Action action);
+
     void stopAllActions();
+
     void stopActionsIf(Predicate<Action> removalPredicate);
 
     void setDefaultAction(Subsystem subsystem, Action action);
+
     Optional<Action> getActionRunningOnSubsystem(Subsystem subsystem);
 
-	void run(RobotMode robotMode);
+    void run(RobotMode robotMode);
 }
