2f879d166c677004fb1ac1c8d7f1eb996275f3ba
Tom Tzook <tomtzook@gmail.com>
2019-11-16 16:39:45 +0200

    additional benchmarks

---
+++ flashlibj/src/jmh/java/com/flash3388/flashlib/robot/scheduling/EmptySubsystem.java
@@ -0,0 +1,8 @@
+package com.flash3388.flashlib.robot.scheduling;
+
+public class EmptySubsystem extends Subsystem {
+
+    public EmptySubsystem(Scheduler scheduler) {
+        super(scheduler);
+    }
+}
