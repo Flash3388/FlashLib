d0f4ec2590535432323b7c7c1cde4a5561a2cde3
Tom Tzook <tomtzook@gmail.com>
2019-11-27 19:25:43 +0200

    trying csv source

--- flashutil/src/test/java/com/flash3388/flashlib/math/MathfTest.java
+++ flashutil/src/test/java/com/flash3388/flashlib/math/MathfTest.java
@@ -1,10 +1,7 @@
 package com.flash3388.flashlib.math;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,60 +10,51 @@ public class MathfTest {
     private static final double EQUAL_MARGIN = 0.0001;
 
     @ParameterizedTest(name = "translateAngle({0}) = {1}")
-    @MethodSource(value = "translateAngleArguments")
+    @CsvSource({
+            "0.0, 0.0",
+            "30.0, 30.0",
+            "180.0, 180.0",
+            "280.0, 280.0",
+            "360.0, 0.0",
+            "-180.0, 180.0",
+            "480.0, 120.0",
+            "1080.0, 0.0",
+            "-480.0, 240.0"
+    })
     public void translateAngle_ofParameters_producesExpectedResult(double angleToTranslate, double expectedResult) throws Exception {
         double translated = Mathf.translateAngle(angleToTranslate);
         assertEquals(expectedResult, translated, EQUAL_MARGIN);
     }
 
     @ParameterizedTest(name = "translateInRange({0}, {1}, {2}) = {3}")
-    @MethodSource(value = "translateInRangeArguments")
+    @CsvSource({
+            "0.0, 360.0, true, 0.0",
+            "30.0, 360.0, true, 30.0",
+            "180.0, 360.0, true, 180.0",
+            "280.0, 360.0, true, 280.0",
+            "360.0, 360.0, true, 0.0",
+            "-180.0, 360.0, true, 180.0",
+            "480.0, 360.0, true, 120.0",
+            "1080.0, 360.0, true, 0.0",
+            "-480.0, 360.0, true, 240.0",
+            "-480.0, 360.0, false, -120.0",
+            "-100.0, 5.0, true, 0.0"
+    })
     public void translateAngle_ofParameters_producesExpectedResult(double valueToTranslate, double rangeToTranslateIn, boolean shouldForcePositive, double expectedResult) throws Exception {
         double translated = Mathf.translateInRange(valueToTranslate, rangeToTranslateIn, shouldForcePositive);
         assertEquals(expectedResult, translated, EQUAL_MARGIN);
     }
 
     @ParameterizedTest(name = "shortestAngularDistance({0}, {1}) = {2}")
-    @MethodSource(value = "angularDistanceArguments")
+    @CsvSource({
+            "0.0, 0.0, 0.0",
+            "180.0, 0.0, 180.0",
+            "270.0, 0.0, 90.0",
+            "90.0, 360.0, 90.0",
+            "90.0, 0.0, 90.0"
+    })
     public void shortestAngularDistance_ofParameters_producesExpectedResult(double current, double last, double expectedResult) throws Exception {
         double distance = Mathf.shortestAngularDistance(current, last);
         assertEquals(expectedResult, distance, EQUAL_MARGIN);
-    }
-
-    private static Stream<Arguments> translateAngleArguments() {
-        return Stream.of(
-                Arguments.of(0.0, 0.0),
-                Arguments.of(30.0, 30.0),
-                Arguments.of(180.0, 180.0),
-                Arguments.of(280.0, 280.0),
-                Arguments.of(360.0, 0.0),
-                Arguments.of(-180.0, 180.0),
-                Arguments.of(480.0, 120.0),
-                Arguments.of(1080.0, 0.0),
-                Arguments.of(-480.0, 240.0));
-    }
-
-    private static Stream<Arguments> translateInRangeArguments() {
-        return Stream.of(
-                Arguments.of(0.0, 360.0, true, 0.0),
-                Arguments.of(30.0, 360.0, true, 30.0),
-                Arguments.of(180.0, 360.0, true, 180.0),
-                Arguments.of(280.0, 360.0, true, 280.0),
-                Arguments.of(360.0, 360.0, true, 0.0),
-                Arguments.of(-180.0, 360.0, true, 180.0),
-                Arguments.of(480.0, 360.0, true, 120.0),
-                Arguments.of(1080.0, 360.0, true, 0.0),
-                Arguments.of(-480.0, 360.0, true, 240.0),
-                Arguments.of(-480.0, 360.0, false, -120.0),
-                Arguments.of(-100.0, 5.0, true, 0.0));
-    }
-
-    private static Stream<Arguments> angularDistanceArguments() {
-        return Stream.of(
-                Arguments.of(0.0, 0.0, 0.0),
-                Arguments.of(180.0, 0.0, 180.0),
-                Arguments.of(270.0, 0.0, 90.0),
-                Arguments.of(90.0, 360.0, 90.0),
-                Arguments.of(90.0, 0.0, 90.0));
     }
 }
