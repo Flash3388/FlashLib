77aa51c26e054ebcb5e01d95d1fee4e6c4bc528e
Tom Tzook <tomtzook@gmail.com>
2020-08-26 04:40:52 +0300

    movable2d extends movable

--- flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/motion/Movable2d.java
+++ flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/motion/Movable2d.java
@@ -1,10 +1,14 @@
 package com.flash3388.flashlib.robot.motion;
 
-import com.flash3388.flashlib.control.Stoppable;
 import com.flash3388.flashlib.scheduling.Requirement;
 import com.jmath.vectors.Vector2;
 
-public interface Movable2d extends Stoppable, Requirement {
+public interface Movable2d extends Movable, Requirement {
 
     void move(Vector2 motionVector);
+
+    @Override
+    default void move(double speed) {
+        move(Vector2.polar(speed, 0.0));
+    }
 }
