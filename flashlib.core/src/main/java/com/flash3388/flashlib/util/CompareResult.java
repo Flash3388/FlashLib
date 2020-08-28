f6ca0314fe30dc7c821c81d0b9b4dceae5de74ce
Tom Tzook <tomtzook@gmail.com>
2020-08-28 05:35:11 +0300

    some documentation

--- flashlib.core/src/main/java/com/flash3388/flashlib/util/CompareResult.java
+++ flashlib.core/src/main/java/com/flash3388/flashlib/util/CompareResult.java
@@ -1,8 +1,23 @@
 package com.flash3388.flashlib.util;
 
+/**
+ * Represents the results from {@link Comparable#compareTo(Object)} or
+ * {@link java.util.Comparator#compare(Object, Object)}.
+ *
+ * @since FlashLib 2.0.0
+ */
 public enum CompareResult {
+    /**
+     * If the first value (or this) was greater than the other.
+     */
     GREATER_THAN(1),
+    /**
+     * If the first value (or this) was smaller than the other.
+     */
     SMALLER_THAN(-1),
+    /**
+     * If the first value (or this) was equal to the other.
+     */
     EQUAL_TO(0);
 
     private final int mValue;
@@ -11,24 +26,79 @@ public enum CompareResult {
         mValue = value;
     }
 
+    /**
+     * The sign returned by comparing methods.
+     *
+     * @return 1, -1 or 0 for greater than, smaller than or equal to.
+     */
     public int value() {
         return mValue;
     }
 
+    /**
+     * Gets whether {@link #value()} matches the sign of the given value.
+     * <p>
+     *     Helpful when comparing against results from a compare method:
+     * </p>
+     * <pre>
+     *     int result = something.compareTo(other);
+     *     if (CompareResult.EQUAL_TO.is(result)) {
+     *         // do something
+     *     }
+     * </pre>
+     *
+     * @param value value to compare against.
+     *
+     * @return <b>true</b> if equal, <b>false</b> otherwise.
+     */
     public boolean is(int value) {
-        return mValue == value;
+        return mValue == Math.signum(value);
     }
 
+    /**
+     * Finds the {@link CompareResult} object which matches the given comparing result
+     * value.
+     * <p>
+     *     Can be used directly against a compare results to get the enum value:
+     * </p>
+     * <pre>
+     *     int result = something.compareTo(other);
+     *     CompareResult compareResult = CompareResult.forValue(result);
+     * </pre>
+     *
+     * @param value 1, -1 or 0, as produced by {@link Comparable#compareTo(Object)} and
+     *  {@link java.util.Comparator#compare(Object, Object)}.
+     *
+     * @return a {@link CompareResult} whose {@link #value()} matches the given value.
+     */
     public static CompareResult forValue(int value) {
         for (CompareResult result : values()) {
-            if (result.value() == value) {
+            if (result.is(value)) {
                 return result;
             }
         }
 
         throw new EnumConstantNotPresentException(CompareResult.class, String.valueOf(value));
     }
-    
+
+    /**
+     * Checks whether or not the given value matches the {@link #value()} of one of the given {@link CompareResult}.
+     * In essence, checking if the given result, is one of the expected possible.
+     * <p>
+     *     Can be helpful if checking whether or not a compare result matches several options:
+     * </p>
+     * <pre>
+     *     int result = something.compareTo(other);
+     *     if (CompareResult.in(result, CompareResult.EQUAL_TO, CompareResult.GREATER_THAN)) {
+     *         // Do something
+     *     }
+     * </pre>
+     *
+     * @param value value to check.
+     * @param possibleResults accepted results.
+     *
+     * @return <b>true</b> if the given value matches on of the accepted results, <b>false</b> otherwise.
+     */
     public static boolean in(int value, CompareResult... possibleResults) {
         for (CompareResult compareResult : possibleResults) {
             if (compareResult.is(value)) {
--- flashlib.core/src/main/java/com/flash3388/flashlib/util/concurrent/Sleeper.java
+++ flashlib.core/src/main/java/com/flash3388/flashlib/util/concurrent/Sleeper.java
@@ -5,17 +5,52 @@ import com.flash3388.flashlib.time.Time;
 
 import java.util.function.BooleanSupplier;
 
+/**
+ * Utility class for sleeping operations.
+ *
+ * @since FlashLib 2.0.0
+ */
 public class Sleeper {
 
     static final long MIN_SLEEP_PERIOD_MS = 5;
     private static final long MAX_SLEEP_PERIOD_MS = 1000;
 
-    public void sleep(Time sleepTime) throws InterruptedException {
-        sleepMs(sleepTime.valueAsMillis());
+    /**
+     * Puts the current thread to sleep for the given amount of time.
+     *
+     * @param duration duration of sleep.
+     *
+     * @throws InterruptedException if the current thread is interrupted.
+     *
+     * @see Thread#sleep(long)
+     */
+    public void sleep(Time duration) throws InterruptedException {
+        sleepMs(duration.valueAsMillis());
     }
 
-    public void sleepWhileConditionMet(BooleanSupplier condition, Time sleepTime) throws InterruptedException {
-        long timeoutMs = sleepTime.valueAsMillis();
+    /**
+     * Puts the current to thread to sleep for a given amount of time, or until the given condition is no
+     * longer <b>true</b>.
+     * <p>
+     *     The actual sleeping is not continuous, but rather broken up into section. This allows
+     *     to sample that condition between each sleep section. This also means that the sleeping
+     *     is not entirely accurate.
+     * </p>
+     * <p>
+     *     For more accurate waking up and sleeping, it is based to used a solution based on locks,
+     *     or utilize java's {@link java.util.concurrent.ExecutorService} and {@link java.util.concurrent.Future}
+     *     API's (depending on the need).
+     * </p>
+     *
+     * @param condition condition to listen to.
+     * @param duration duration of sleep.
+     *
+     * @throws InterruptedException if the current thread is interrupted.
+     *
+     * @see Thread#sleep(long)
+     */
+    public void sleepWhileConditionMet(BooleanSupplier condition, Time duration) throws InterruptedException {
+        long timeoutMs = duration.valueAsMillis();
         long sleepingPeriodMs = getSleepingPeriodMs(timeoutMs);
         long timeWaited = 0;
 
