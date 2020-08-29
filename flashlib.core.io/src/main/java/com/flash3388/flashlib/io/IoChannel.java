16078ee4f29428f5e63778b49f565ac99b8e4904
Tom Tzook <tomtzook@gmail.com>
2020-08-29 17:37:43 +0300

    finished docs in IO for now

--- flashlib.core.io/src/main/java/com/flash3388/flashlib/io/IoChannel.java
+++ flashlib.core.io/src/main/java/com/flash3388/flashlib/io/IoChannel.java
@@ -1,7 +1,37 @@
 package com.flash3388.flashlib.io;
 
+/**
+ * Represents a connection to an io port. Used with {@link IoInterface}
+ * as a key for identifying specific devices.
+ *
+ * The actual object to create depends on the implementation used for the API. Allowing
+ * each implementation to define different requirements for the channel.
+ *
+ * @since FlashLib 3.0.0
+ */
 public interface IoChannel {
 
+    /**
+     * Casts the {@link IoChannel} to the requested type.
+     * <p>
+     *     Should be used by implementations in {@link IoInterface} to cast the given
+     *     object to the wanted type.
+     * </p>
+     * <pre>
+     *     void newChannel(IoChannel channel) {
+     *         MyIoChannel myChannel = IoChannel.cast(channel, MyIoChannel.cast);
+     *         myChannel.getData();
+     *     }
+     * </pre>
+     *
+     * @param channel the channel object.
+     * @param type the wanted class type.
+     * @param <T> type parameter indicating the wanted class.
+     *
+     * @return a casted {@link IoChannel}.
+     *
+     * @throws ClassCastException if the given instance cannot be casted to the wanted type.
+     */
     static <T extends IoChannel> T cast(IoChannel channel, Class<T> type) {
         if (type.isInstance(channel)) {
             return type.cast(channel);
--- flashlib.core.io/src/main/java/com/flash3388/flashlib/io/IoInterface.java
+++ flashlib.core.io/src/main/java/com/flash3388/flashlib/io/IoInterface.java
@@ -1,13 +1,88 @@
 package com.flash3388.flashlib.io;
 
+/**
+ * A connection layer to Input/Output ports. Capable of creating connection and allowing access to different IO
+ * devices and ports.
+ * What ports can be accessed depends on the implementations.
+ * <p>
+ *     The {@link IoChannel} objects passed to this interface, must match the ones wanted by the implementation.
+ *     Refer to the specific implementation used to verify how to retrieve/create those.
+ * </p>
+ *
+ * @since FlashLib 3.0.0
+ */
 public interface IoInterface {
 
+    /**
+     * Creates a new {@link AnalogInput} representing an analog port for input defined by the given
+     * {@link IoChannel}.
+     *
+     * @param channel the channel describing the connection.
+     *
+     * @return {@link AnalogInput} object matching the control described by the given {@link IoChannel}.
+     *
+     * @throws IllegalArgumentException if the given channel does not exist or does not correspond to a control
+     *  representable by {@link AnalogInput}.
+     */
     AnalogInput newAnalogInput(IoChannel channel);
+
+    /**
+     * Creates a new {@link AnalogOutput} representing an analog port for output defined by the given
+     * {@link IoChannel}.
+     *
+     * @param channel the channel describing the connection.
+     *
+     * @return {@link AnalogOutput} object matching the control described by the given {@link IoChannel}.
+     *
+     * @throws IllegalArgumentException if the given channel does not exist or does not correspond to a control
+     *  representable by {@link AnalogOutput}.
+     */
     AnalogOutput newAnalogOutput(IoChannel channel);
+
+    /**
+     * Creates a new {@link DigitalInput} representing an digital port for input defined by the given
+     * {@link IoChannel}.
+     *
+     * @param channel the channel describing the connection.
+     *
+     * @return {@link DigitalInput} object matching the control described by the given {@link IoChannel}.
+     *
+     * @throws IllegalArgumentException if the given channel does not exist or does not correspond to a control
+     *  representable by {@link DigitalInput}.
+     */
     DigitalInput newDigitalInput(IoChannel channel);
+
+    /**
+     * Creates a new {@link DigitalOutput} representing an digital port for output defined by the given
+     * {@link IoChannel}.
+     *
+     * @param channel the channel describing the connection.
+     *
+     * @return {@link DigitalOutput} object matching the control described by the given {@link IoChannel}.
+     *
+     * @throws IllegalArgumentException if the given channel does not exist or does not correspond to a control
+     *  representable by {@link DigitalOutput}.
+     */
     DigitalOutput newDigitalOutput(IoChannel channel);
+
+    /**
+     * Creates a new {@link Pwm} representing an PWM port for output defined by the given
+     * {@link IoChannel}.
+     *
+     * @param channel the channel describing the connection.
+     *
+     * @return {@link Pwm} object matching the control described by the given {@link IoChannel}.
+     *
+     * @throws IllegalArgumentException if the given channel does not exist or does not correspond to a control
+     *  representable by {@link Pwm}.
+     */
     Pwm newPwm(IoChannel channel);
 
+    /**
+     * A stub implementation of {@link IoInterface}. All the methods throw {@link UnsupportedOperationException}.
+     *
+     * @since FlashLib 3.0.0
+     */
     class Stub implements IoInterface {
 
         @Override
