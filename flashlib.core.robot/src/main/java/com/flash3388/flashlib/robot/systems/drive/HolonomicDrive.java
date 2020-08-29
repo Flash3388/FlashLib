b1b13697b2b5554aa942f48b43df6e9bbc92e814
Tom Tzook <tomtzook@gmail.com>
2020-08-29 21:41:00 +0300

    more javadoc

--- flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/systems/drive/HolonomicDrive.java
+++ flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/systems/drive/HolonomicDrive.java
@@ -5,8 +5,7 @@ import com.jmath.vectors.Vector2;
 
 /**
  * Interface for holonomic drive systems, i.e. Mecanum and Omni. Extends {@link Drive}.
- * 
- * @author Tom Tzook
+ *
  * @since FlashLib 1.0.0
  */
 public interface HolonomicDrive extends Drive, Movable2d {
@@ -42,10 +41,22 @@ public interface HolonomicDrive extends Drive, Movable2d {
      */
     void holonomicCartesian(double y, double x, double rotation);
 
+    /**
+     * Moves the drive system using a 2d motion vector and a rotation value.
+     *
+     * @param vector 2d motion vector describing motion along y and x axes.
+     * @param rotation rotation value [-1...1] describing rotation modifier
+     *                 to the motion.
+     */
     default void holonomicDrive(Vector2 vector, double rotation) {
         holonomicPolar(vector.magnitude(), vector.angle(), rotation);
     }
 
+    /**
+     * Moves the drive system.
+     *
+     * @param driveSpeed drive speeds.
+     */
     default void holonomicDrive(HolonomicDriveSpeed driveSpeed) {
         holonomicDrive(driveSpeed.getDriveVector(), driveSpeed.getRotation());
     }
