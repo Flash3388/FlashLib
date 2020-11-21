b06db4412c68a8e60253b0edd739973b134c09be
Tom Tzook <tomtzook@gmail.com>
2020-11-21 02:26:06 +0200

    some javadoc updates

--- flashlib.core.scheduling/src/main/java/com/flash3388/flashlib/scheduling/Scheduler.java
+++ flashlib.core.scheduling/src/main/java/com/flash3388/flashlib/scheduling/Scheduler.java
@@ -7,16 +7,13 @@ import java.util.Optional;
 import java.util.function.Predicate;
 
 /**
- * Scheduler is responsible for executing tasks for robots.
+ * Scheduler is the executor for FlashLib's scheduling component. It is responsible for executing all
+ * running actions (as described by {@link Action}) and managing their requirements, ensuring that there's no conflict.
  * <p>
- * For more complex operations, the scheduler can use {@link Action} objects. Those objects can be added
- * to the scheduler and then executed as well. Unlike simple tasks, scheduling might depend on {@link Requirement}
- * objects for operations. The scheduler tracks the required systems of each action making sure that only one
- * {@link Action} object runs on a {@link Requirement} at any given time.
- * <p>
- * In addition, the scheduler can allow {@link Subsystem} to hold default {@link Action} objects, which
- * run only if no {@link Action} is using the {@link Subsystem} at the moment. When the scheduler runs,
- * it checks all registered {@link Requirement} objects to see if one does not have an action at the moment.
+ *     In addition, the scheduler provides support for default {@link Subsystem} actions, executing them when
+ *     no other actions are running for the given subsystem.
+ * </p>
+ *
  *
  * @since FlashLib 1.0.0
  */
