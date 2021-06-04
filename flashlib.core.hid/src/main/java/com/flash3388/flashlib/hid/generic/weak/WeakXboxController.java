df31b944e4a40dc63d200a23d0e75e65bbde49ca
Tom Tzook <tomtzook@gmail.com>
2021-06-05 00:51:38 +0300

    generic offshoot -> weak hid

---
+++ flashlib.core.hid/src/main/java/com/flash3388/flashlib/hid/generic/weak/WeakXboxController.java
@@ -0,0 +1,56 @@
+package com.flash3388.flashlib.hid.generic.weak;
+
+import com.flash3388.flashlib.hid.Axis;
+import com.flash3388.flashlib.hid.Button;
+import com.flash3388.flashlib.hid.Dpad;
+import com.flash3388.flashlib.hid.Pov;
+import com.flash3388.flashlib.hid.XboxAxis;
+import com.flash3388.flashlib.hid.XboxController;
+import com.flash3388.flashlib.hid.generic.GenericAxis;
+import com.flash3388.flashlib.hid.generic.GenericButton;
+import com.flash3388.flashlib.hid.generic.GenericDpad;
+import com.flash3388.flashlib.hid.generic.RawHidInterface;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class WeakXboxController extends WeakHid implements XboxController {
+
+    public WeakXboxController(RawHidInterface hidInterface, int channel) {
+        super(hidInterface, channel,
+                createAxes(hidInterface, channel),
+                createButtons(hidInterface, channel),
+                Collections.singletonList(new GenericDpad(hidInterface, channel, 0)));
+    }
+
+    @Override
+    public Pov getPov(int index) {
+        if (index != 0) {
+            throw new IllegalArgumentException("Unknown pov " + index);
+        }
+
+        return super.getPov(index);
+    }
+
+    @Override
+    public Dpad getDpad() {
+        return (Dpad) getPov(0);
+    }
+
+    private static List<Axis> createAxes(RawHidInterface hidInterface, int channel) {
+        List<Axis> axes = new ArrayList<>();
+        for (int i = 0; i < XboxAxis.count(); i++) {
+            axes.add(new GenericAxis(hidInterface, channel, i));
+        }
+        return axes;
+    }
+
+    private static List<Button> createButtons(RawHidInterface hidInterface, int channel) {
+        List<Button> buttons = new ArrayList<>();
+        for (int i = 0; i < XboxAxis.count(); i++) {
+            buttons.add(new GenericButton(hidInterface, channel, i));
+        }
+        return buttons;
+    }
+}
