80196bfd7d90110716903f4b5290889f6bfebe3e
Tom Tzook <tomtzook@gmail.com>
2021-06-05 01:10:48 +0300

    constant speed

---
+++ flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/systems/ConstantSpeedMotorSystem.java
@@ -0,0 +1,44 @@
+package com.flash3388.flashlib.robot.systems;
+
+import com.flash3388.flashlib.control.Direction;
+import com.flash3388.flashlib.io.devices.SpeedController;
+import com.flash3388.flashlib.io.devices.SpeedControllerGroup;
+
+public class ConstantSpeedMotorSystem extends MotorSystem {
+
+    private final double mSpeed;
+
+    public ConstantSpeedMotorSystem(SpeedController controller, double speed) {
+        super(controller);
+        mSpeed = speed;
+    }
+
+    public ConstantSpeedMotorSystem(double speed, SpeedController... controllers) {
+        super(new SpeedControllerGroup(controllers));
+        mSpeed = speed;
+    }
+
+    public void move(Direction direction) {
+        move(mSpeed, direction);
+    }
+
+    public void forward() {
+        forward(mSpeed);
+    }
+
+    public void backward() {
+        backward(mSpeed);
+    }
+
+    public void rotate(Direction direction) {
+        rotate(mSpeed, direction);
+    }
+
+    public void rotateRight() {
+        rotate(Direction.FORWARD);
+    }
+
+    public void rotateLeft() {
+        rotate(Direction.BACKWARD);
+    }
+}
