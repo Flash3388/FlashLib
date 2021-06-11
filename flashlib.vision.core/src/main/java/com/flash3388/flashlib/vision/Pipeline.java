5af82cfc1dfa82a8ae370f0342556a1ef41a45d0
Tom Tzook <tomtzook@gmail.com>
2021-06-11 22:17:42 +0300

    filtering

--- flashlib.vision.core/src/main/java/com/flash3388/flashlib/vision/Pipeline.java
+++ flashlib.vision.core/src/main/java/com/flash3388/flashlib/vision/Pipeline.java
@@ -4,6 +4,7 @@ import com.castle.util.throwables.ThrowableHandler;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 @FunctionalInterface
 public interface Pipeline<T> {
@@ -15,7 +16,11 @@ public interface Pipeline<T> {
     }
 
     default <T2> Pipeline<T> mapTo(Pipeline<? super T2> pipeline, Function<? super T, ? extends T2> mapper) {
-        return new MappingPipeline<>(pipeline, mapper);
+        return divergeTo(new MappingPipeline<>(pipeline, mapper));
+    }
+
+    default Pipeline<T> filterTo(Predicate<? super T> filter, Pipeline<? super T> pipeline) {
+        return divergeTo(new FilterPipeline<>(filter, pipeline));
     }
 
     default Consumer<T> asConsumer(ThrowableHandler throwableHandler) {
