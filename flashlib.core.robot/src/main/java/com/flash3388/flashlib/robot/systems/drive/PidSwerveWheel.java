4502b0370c0afe114b6002bb635292bb7a46f34c
dandeduck <danielmkhlv@gmail.com>
2021-08-30 19:52:05 +0300

    added un implemented classes

---
+++ flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/systems/drive/PidSwerveWheel.java
@@ -0,0 +1,22 @@
+package com.flash3388.flashlib.robot.systems.drive;
+
+public class PidSwerveWheel implements SwerveWheel {
+
+    @Override
+    public void move(double speed) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void rotate(double speed) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        
+    }
+}
---
+++ flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/systems/drive/SwerveWheel.java
@@ -0,0 +1,7 @@
+package com.flash3388.flashlib.robot.systems.drive;
+
+import com.flash3388.flashlib.robot.motion.Movable;
+import com.flash3388.flashlib.robot.motion.Rotatable;
+
+public interface SwerveWheel extends Movable, Rotatable {
+}
