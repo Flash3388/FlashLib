3b0801129460a73419d533ee3a43da6b9157e527
tomtzook <tomtzook@gmail.com>
2022-07-29 22:12:55 +0300

    new scheduler is default for robots

--- flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/RobotFactory.java
+++ flashlib.core.robot/src/main/java/com/flash3388/flashlib/robot/RobotFactory.java
@@ -2,8 +2,10 @@ package com.flash3388.flashlib.robot;
 
 import com.flash3388.flashlib.scheduling.Scheduler;
 import com.flash3388.flashlib.scheduling.SynchronousScheduler;
+import com.flash3388.flashlib.scheduling.impl.NewSynchronousScheduler;
 import com.flash3388.flashlib.time.Clock;
 import com.flash3388.flashlib.time.SystemNanoClock;
+import com.flash3388.flashlib.util.logging.Logging;
 import org.slf4j.Logger;
 
 public final class RobotFactory {
@@ -11,11 +13,11 @@ public final class RobotFactory {
     private RobotFactory() {}
 
     public static Scheduler newDefaultScheduler(Clock clock) {
-        return new SynchronousScheduler(clock);
+        return new NewSynchronousScheduler(clock, Logging.stub());
     }
 
     public static Scheduler newDefaultScheduler(Clock clock, Logger logger) {
-        return new SynchronousScheduler(clock, logger);
+        return new NewSynchronousScheduler(clock, logger);
     }
 
     public static Clock newDefaultClock() {
