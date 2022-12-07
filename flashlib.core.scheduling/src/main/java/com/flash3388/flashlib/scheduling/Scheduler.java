6cdd30049e1e61aa977420748014093402be80f8
tomtzook <tomtzook@gmail.com>
2022-12-07 21:11:42 +0200

    some docs

--- flashlib.core.scheduling/src/main/java/com/flash3388/flashlib/scheduling/Scheduler.java
+++ flashlib.core.scheduling/src/main/java/com/flash3388/flashlib/scheduling/Scheduler.java
@@ -164,7 +164,22 @@ public interface Scheduler {
      */
     Trigger newTrigger(BooleanSupplier condition);
 
+    /**
+     * Creates a context for running actions. Can be used to manually execute actions. Should be
+     * used carefully as no requirements checks are made on the action.
+     * Generally used by groups to run actions.
+     *
+     * @param group group which contains the action.
+     * @param action action to use.
+     * @return execution context.
+     */
     ExecutionContext createExecutionContext(ActionGroup group, Action action);
 
+    /**
+     * Creates a new group for executes actions.
+     *
+     * @param type type of group to create. Influences the execution order and flow.
+     * @return action group.
+     */
     ActionGroup newActionGroup(ActionGroupType type);
 }
