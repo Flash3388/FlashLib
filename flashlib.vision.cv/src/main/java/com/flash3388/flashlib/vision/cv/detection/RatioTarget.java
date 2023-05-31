2c849ae9049a7192901097b403240208ef64a2f3
Apathetic Idiot <oren4879@gmail.com>
2023-05-31 14:49:42 +0300

    RatioTarget extends RectTarget made RatioTarget extend RectTarget

--- flashlib.vision.cv/src/main/java/com/flash3388/flashlib/vision/cv/detection/RatioTarget.java
+++ flashlib.vision.cv/src/main/java/com/flash3388/flashlib/vision/cv/detection/RatioTarget.java
@@ -2,40 +2,19 @@ package com.flash3388.flashlib.vision.cv.detection;
 
 import com.flash3388.flashlib.vision.detection.RealTargetConfig;
 import com.flash3388.flashlib.vision.detection.ScorableTarget;
-import com.jmath.vectors.Vector2;
 import org.opencv.core.Rect;
 
-public class RatioTarget implements ScorableTarget {
-
-    private final Rect mRect;
+public class RatioTarget extends RectTarget implements ScorableTarget {
     private final RealTargetConfig mRealTargetConfig;
 
     public RatioTarget(Rect rect, RealTargetConfig realTargetConfig) {
-        mRect = rect;
+        super(rect);
         mRealTargetConfig = realTargetConfig;
     }
 
     @Override
-    public int getWidthPixels() {
-        return mRect.width;
-    }
-
-    @Override
-    public int getHeightPixels() {
-        return mRect.height;
-    }
-
-    @Override
-    public Vector2 getCenter() {
-        return new Vector2(
-                mRect.x + mRect.width * 0.5,
-                mRect.y + mRect.height * 0.5
-        );
-    }
-
-    @Override
     public double score() {
-        double ratio = mRect.width / (double) mRect.height;
+        double ratio = rect.width / (double) rect.height;
         return ratio > mRealTargetConfig.getDimensionsRatio() ?
                 mRealTargetConfig.getDimensionsRatio() / ratio :
                 ratio / mRealTargetConfig.getDimensionsRatio();
