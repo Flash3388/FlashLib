b97f26ae45d078456e9c5caf978e06015d9073ed
Apathetic Idiot <oren4879@gmail.com>
2023-05-31 13:56:26 +0300

    Inverted Range added an attribute to DimensionRange that tells whether the range is inverted or not

--- flashlib.vision.core/src/main/java/com/flash3388/flashlib/vision/color/DimensionRange.java
+++ flashlib.vision.core/src/main/java/com/flash3388/flashlib/vision/color/DimensionRange.java
@@ -5,15 +5,27 @@ public class DimensionRange {
     private final ColorDimension mDimension;
     private final int mMin;
     private final int mMax;
+    private final boolean mInverted;
+
+    public DimensionRange(ColorDimension dimension, int min, int max, boolean inverted) {
+        mDimension = dimension;
+        mMin = min;
+        mMax = max;
+        mInverted = inverted;
+    }
 
     public DimensionRange(ColorDimension dimension, int min, int max) {
         mDimension = dimension;
         mMin = min;
         mMax = max;
+        mInverted = false;
     }
-
     public ColorDimension getDimension() {
         return mDimension;
+    }
+
+    public boolean isInverted() {
+        return mInverted;
     }
 
     public int getMin() {
